27d1953f9095269728c3a67a39a4b004b664a2fd
Alexander <mrbeachcode@gmail.com>
2019-10-04 16:02:17 -0400

    Made the project with the dependencies.

---
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/ProjecttwobaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttwobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttwobaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttwobaseApplication.class, args);
+	}
+
+}
