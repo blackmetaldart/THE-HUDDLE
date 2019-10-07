b6289a77756b04ae5a52696164f091e96eec01e3
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 11:18:13 -0400

    Finished Comment Service  (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/CommentService.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/CommentService.java
@@ -1,4 +1,10 @@
 package com.example.projecttwobase.service;
 
+import com.example.projecttwobase.model.Comment;
+
 public interface CommentService {
+    public Comment createComment(Comment comment);
+    public Comment getCommentByCommentId (Long commentId);
+    public Comment getCommentByPostId (Long postId);
+    public Comment deleteCommentByCommentId (Long commentId);
 }
