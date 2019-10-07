abd5b3d67c3a5f8edec6150ae59ec2ec2fdee134
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 10:01:54 -0400

    Added User Profile to all packages

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/config/SecurityConfig.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/config/SecurityConfig.java
@@ -2,3 +2,4 @@ package com.example.projecttwobase.config;
 
 public class SecurityConfig {
 }
+
---
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/UserProfileController.java
@@ -0,0 +1,4 @@
+package com.example.projecttwobase.controller;
+
+public class UserProfileController {
+}
