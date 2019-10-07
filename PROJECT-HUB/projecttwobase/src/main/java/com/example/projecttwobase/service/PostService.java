9c78bd651cbcf2036a8be600f8b5ae312cc2592b
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 11:12:39 -0400

    Finished Post Service (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostService.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostService.java
@@ -1,4 +1,11 @@
 package com.example.projecttwobase.service;
 
+import com.example.projecttwobase.model.Post;
+
 public interface PostService {
+    public Post createPost(Post post);
+    public void deletePost(Long postId);
+    public Post getPost();
+    public Iterable<Post> listPost();
+
 }
--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostServiceImpl.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostServiceImpl.java
@@ -1,4 +1,7 @@
 package com.example.projecttwobase.service;
 
+import com.example.projecttwobase.model.Post;
+
 public class PostServiceImpl {
+
 }
