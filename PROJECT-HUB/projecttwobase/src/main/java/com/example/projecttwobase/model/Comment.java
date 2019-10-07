426225af5332fe5ef388dbc3fdb752ad889ac046
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 10:31:13 -0400

    Comment Model complete!

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/Comment.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/Comment.java
@@ -1,4 +1,21 @@
 package com.example.projecttwobase.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comment")
 public class Comment {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String text;
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getText() {return text;}
+    public void setText(String text) {this.text = text;}
 }
