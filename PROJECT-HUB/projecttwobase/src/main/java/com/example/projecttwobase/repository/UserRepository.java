a0dd5c32e62c50e146e97495bb584048f27890d3
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 12:18:59 -0400

    FINISHED User Repository (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/repository/UserRepository.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.projecttwobase.repository;
 
 
 import com.example.projecttwobase.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    public User findByUsername (String username);
+
+    @Query("FROM User u WHERE u.username = ?1 and u.password = ?2")
+    public User login (String username, String password);
 
 
 }
