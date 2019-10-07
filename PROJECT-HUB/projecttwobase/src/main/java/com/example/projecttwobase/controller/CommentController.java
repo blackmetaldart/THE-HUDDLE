80d6c67e3a0056248670eee0b12796ba1c0dd500
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 11:51:13 -0400

    Finished Comment Controller (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/CommentController.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/CommentController.java
@@ -1,4 +1,21 @@
 package com.example.projecttwobase.controller;
 
+import com.example.projecttwobase.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comment")
 public class CommentController {
+    @Autowired
+    CommentService commentService;
+
+    //POST(create) COMMENT
+    @PostMapping
+
+    //GET COMMENT
+    @GetMapping("/{id}")
+
+    //DELETE COMMENT
+    @DeleteMapping("/{id}")
 }
