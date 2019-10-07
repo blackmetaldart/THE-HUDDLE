270201367880312896bb98c241c256f13479310f
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 10:24:08 -0400

    User Profile Model complete!

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/UserProfile.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/UserProfile.java
@@ -1,4 +1,30 @@
 package com.example.projecttwobase.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "userProfile")
 public class UserProfile {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String addtEmail;
+
+    @Column
+    private String mobile;
+
+    @Column
+    private String address;
+
+    public String getAddtEmail() {return addtEmail;}
+    public void setAddtEmail(String addtEmail) {this.addtEmail = addtEmail;}
+
+    public String getMobile() {return mobile;}
+    public void setMobile(String mobile) {this.mobile = mobile;}
+
+    public String getAddress() {return address;}
+    public void setAddress(String address) {this.address = address;}
 }
