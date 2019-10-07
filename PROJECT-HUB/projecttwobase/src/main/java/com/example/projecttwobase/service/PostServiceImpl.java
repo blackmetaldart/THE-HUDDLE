95d4751fddfb60e1c28e46bae7685062b2a10264
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 11:21:45 -0400

    Fixed Post Service Impl

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostServiceImpl.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostServiceImpl.java
@@ -1,17 +1,18 @@
 package com.example.projecttwobase.service;
 
 import com.example.projecttwobase.model.Post;
+import com.example.projecttwobase.model.User;
 
 public class PostServiceImpl {
     public Post createPost(Post post){
         return
     }
 
-    public void deletePost(Long postId){
+    public void deletePostByPostId(Long postId){
         return
     }
 
-    public Post getPost() {
+    public Post getPostByUser(User user) {
         return
     }
 
