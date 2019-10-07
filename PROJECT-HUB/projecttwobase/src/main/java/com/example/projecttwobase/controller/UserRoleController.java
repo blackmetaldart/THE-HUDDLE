ac5e6625e9a2257d17ee1758f4848448a7df35ac
Alexander <mrbeachcode@gmail.com>
2019-10-07 15:34:33 -0400

    The Roles are functional

---
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/UserRoleController.java
@@ -0,0 +1,24 @@
+package com.example.projecttwobase.controller;
+
+
+import com.example.projecttwobase.model.UserRole;
+import com.example.projecttwobase.service.UserRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/role")
+public class UserRoleController {
+    @Autowired
+    UserRoleService roleService;
+
+    @GetMapping("/{rolename}")
+    public UserRole getRole(@PathVariable String rolename) {
+        return roleService.getRole(rolename);
+    }
+
+    @PostMapping
+    public UserRole createRole(@RequestBody UserRole role) {
+        return roleService.createRole(role);
+    }
+}
