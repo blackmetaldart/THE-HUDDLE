09bc763ff6b61ca9d67d61cc6267e6b428f95115
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 10:42:05 -0400

    Added public UserProfile(){} to UserModel

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/UserProfile.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/UserProfile.java
@@ -19,6 +19,8 @@ public class UserProfile {
     @Column
     private String address;
 
+    public UserProfile(){}
+
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
 
