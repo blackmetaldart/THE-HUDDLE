4d5af1bb119325a5e2ef568bf158fe7d3dd7b2ca
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 11:45:45 -0400

    Finished Post Controller (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/PostController.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/PostController.java
@@ -1,4 +1,28 @@
 package com.example.projecttwobase.controller;
 
+import com.example.projecttwobase.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/post")
 public class PostController {
+    @Autowired
+    PostService postService;
+
+    //POST
+    @PostMapping
+
+    //GET POST LIST
+    @GetMapping("/list")
+
+    //GET POST BY POST ID
+    @GetMapping("/{id}")
+
+    //GET COMMENT BY POST ID
+    @GetMapping("/{id}/comment")
+
+    //DELETE POST BY ID
+    @DeleteMapping ("/{id}")
 }
+
