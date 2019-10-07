d81e38fea15e411e088fdf99140a1338e658d952
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 11:40:18 -0400

    Finished User Profile Controllr (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/UserProfileController.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/UserProfileController.java
@@ -1,4 +1,23 @@
 package com.example.projecttwobase.controller;
 
+import com.example.projecttwobase.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserProfileController {
+    @Autowired
+    UserProfileService userProfileService;
+
+    //CREATE
+    @PostMapping("/profile")
+
+    //UPDATE
+    @PostMapping("/profile/{username}")
+
+    //GET
+    @GetMapping("/profile/{username}")
+
 }
