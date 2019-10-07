ebe9c9cc6a30bccca89cf8fb9922d0c0d72c30aa
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 10:58:06 -0400

    Finished User Service Impl (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/UserServiceImpl.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/UserServiceImpl.java
@@ -1,4 +1,17 @@
 package com.example.projecttwobase.service;
 
-public class UserServiceImpl {
+import com.example.projecttwobase.model.User;
+
+public class UserServiceImpl implements UserService {
+    public String createUser(User newUser){
+        return "hello";
+    }
+
+    public String login(User user){
+        return "hello";
+    }
+
+    public User getUser(String username) {
+        return
+    }
 }
