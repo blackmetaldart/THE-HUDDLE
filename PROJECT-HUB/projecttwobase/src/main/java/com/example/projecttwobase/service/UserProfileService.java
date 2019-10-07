9679eb212a124acf72bfc340245b99abe989f6dc
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-07 11:04:52 -0400

    Finished User Profile (FOR NOW)

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/UserProfileService.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/UserProfileService.java
@@ -1,4 +1,8 @@
 package com.example.projecttwobase.service;
 
+import com.example.projecttwobase.model.UserProfile;
+
 public interface UserProfileService {
+    public UserProfile createUserProfile (String username, UserProfile newProfile);
+    public UserProfile getUserProfile (String username);s
 }
