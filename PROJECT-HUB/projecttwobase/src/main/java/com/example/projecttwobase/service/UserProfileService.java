63fa158071d65a04e4bc420b5cd8b8940da79d8b
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-08 10:14:58 -0400

    fix user profile service

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/UserProfileService.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/UserProfileService.java
@@ -3,8 +3,7 @@ package com.example.projecttwobase.service;
 import com.example.projecttwobase.model.UserProfile;
 
 public interface UserProfileService {
-    public UserProfile createUserProfile (UserProfile newProfile);
+    public UserProfile createUserProfile (String username, UserProfile newProfile);
     public UserProfile getUserProfile (String username);
     public UserProfile updateUserProfile (String unknown );
-
 }
