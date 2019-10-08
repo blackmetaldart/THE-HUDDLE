c5d2e9b7d5a801ca379c180f2816f79bc836af81
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-08 08:50:18 -0400

    fixed list repository

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/repository/PostRepository.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/repository/PostRepository.java
@@ -3,7 +3,9 @@ package com.example.projecttwobase.repository;
 import com.example.projecttwobase.model.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
-    public Post findPostByUsername (String username);
+    public List<Post> findPostByUsername (String username);
 
 }
