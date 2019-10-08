bd02f3a9a78b0ea2672a248c057db07e9cc9e878
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-08 08:49:36 -0400

    added comment to comment repository

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/repository/CommentRepository.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/repository/CommentRepository.java
@@ -1,11 +1,11 @@
 package com.example.projecttwobase.repository;
 
 import com.example.projecttwobase.model.Comment;
-import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
     public List<Comment> findByPostId(Long postId);
+    public List<Comment> findByUsername(String username);
 }
