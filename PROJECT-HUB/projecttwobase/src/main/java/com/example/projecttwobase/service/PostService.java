a6a8451827ec8729e8f9f790e452bf8f2cfef6c0
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-08 08:51:09 -0400

    fixed post service

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostService.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostService.java
@@ -4,10 +4,12 @@ import com.example.projecttwobase.model.Post;
 import com.example.projecttwobase.model.User;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface PostService {
     public Post createPost(Post post);
     public ResponseEntity deletePostByPostId(Long postId);
-    public Post getPostByUser(User user);
+    public List<Post> getPostByUser(User user);
     public Iterable<Post> listAllPosts();
 
 }
