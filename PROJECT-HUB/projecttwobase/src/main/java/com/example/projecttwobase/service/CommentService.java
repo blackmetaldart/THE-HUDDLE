76dc5a00d21c1c68180854b82e4091f017f4e1a2
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-08 08:53:32 -0400

    fixed comment service again

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/CommentService.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/CommentService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface CommentService {
     public Comment createComment(Comment comment);
-    public List<Comment> getCommentByUser (Long user);
+    public List<Comment> getCommentByUsername (String username);
     public List<Comment> getCommentByPostId (Long postId);
     public void deleteCommentByCommentId (Long commentId);
 }
