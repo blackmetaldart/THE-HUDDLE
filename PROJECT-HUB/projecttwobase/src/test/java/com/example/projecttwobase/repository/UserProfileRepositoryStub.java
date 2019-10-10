d162816a446b982ec46e9947570c7fbfe481a0ec
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:51:36 -0400

    Added to both files in repository test package

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileRepositoryStub.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileRepositoryStub.java
@@ -1,4 +1,70 @@
 package com.example.projecttwobase.repository;
 
-public class UserProfileRepositoryStub {
+
+import com.example.projecttwobase.model.UserProfile;
+import com.example.projecttwobase.repository.UserProfileRepository;
+
+import java.util.Optional;
+
+public class UserProfileRepositoryStub implements UserProfileRepository {
+
+    @Override
+    public <S extends UserProfile> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends UserProfile> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<UserProfile> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<UserProfile> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<UserProfile> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(UserProfile entity) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends UserProfile> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public UserProfile findProfileByUsername(String username) {
+        return null;
+    }
 }
--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
@@ -1,4 +1,9 @@
 package com.example.projecttwobase.repository;
 
+import com.example.projecttwobase.model.UserProfile;
+import com.example.projecttwobase.repository.UserProfileRepositoryStub;
+
+import org.junit.Before;
+
 public class UserProfileServiceTest {
 }
