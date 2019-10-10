ea64b76b8ae17c5306284404754aa64aeb5e8edd
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:02:03 -0400

    Added mockbean to the User Controller Test file

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserControllerTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.projecttwobase.controller;
 
+import com.example.projecttwobase.service.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -30,4 +32,7 @@ public class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World!!"));
     }
+
+    @MockBean
+    private UserService userService;
 }
