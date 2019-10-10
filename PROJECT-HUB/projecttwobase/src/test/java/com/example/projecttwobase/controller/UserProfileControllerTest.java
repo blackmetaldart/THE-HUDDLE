be1e1fd9b09b8f28adcff44ea083cec204ce765d
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:19:14 -0400

    created user profile controller test and added overrides

---
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.projecttwobase.controller;
+
+import com.example.projecttwobase.model.UserProfile;
+
+public class UserProfileControllerTest {
+    @Override
+    public UserProfile createUserProfile(String username, UserProfile newProfile) {
+        return null;
+    }
+
+    @Override
+    public UserProfile getUserProfile(String username) {
+        return null;
+    }
+    private UserProfileController userProfileController;
+
+}
