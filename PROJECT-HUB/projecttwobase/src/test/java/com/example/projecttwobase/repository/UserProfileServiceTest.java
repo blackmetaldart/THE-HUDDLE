90cf9833846f3485d76f27a04b976de5d3818edc
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 15:00:18 -0400

    Worked on user profile service test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
@@ -4,8 +4,12 @@ import com.example.projecttwobase.model.UserProfile;
 import com.example.projecttwobase.repository.UserProfileRepositoryStub;
 
 import com.example.projecttwobase.service.UserProfileServiceImpl;
+import com.example.projecttwobase.service.UserServiceStub;
 import org.junit.Before;
 
 public class UserProfileServiceTest {
-    private UserProfileServiceImpl userProfileService;
+    @Before
+    public void initializeUserProfile(){
+        UserProfileServiceImpl userProfileService = new UserProfileServiceImpl(new UserServiceStub(), new UserProfileRepositoryStub());
+    }
 }
