180a447d09f0a3fca8c2d7d3b25c4810b2720084
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:57:20 -0400

    Added to user profile service test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
@@ -3,7 +3,9 @@ package com.example.projecttwobase.repository;
 import com.example.projecttwobase.model.UserProfile;
 import com.example.projecttwobase.repository.UserProfileRepositoryStub;
 
+import com.example.projecttwobase.service.UserProfileServiceImpl;
 import org.junit.Before;
 
 public class UserProfileServiceTest {
+    private UserProfileServiceImpl userProfileService;
 }
