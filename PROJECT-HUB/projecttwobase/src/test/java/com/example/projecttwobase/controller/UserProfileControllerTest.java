00be355854ca3399aa1e8d8737dd5b1b5505d419
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 17:07:19 -0400

    Fixed bugs caused in testing in user profile controller test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
@@ -1,32 +1,38 @@
 package com.example.projecttwobase.controller;
 
 import com.example.projecttwobase.model.UserProfile;
+import com.example.projecttwobase.repository.UserProfileRepositoryStub;
 import com.example.projecttwobase.service.UserProfileService;
 import com.example.projecttwobase.service.UserProfileServiceImpl;
 import com.example.projecttwobase.service.UserProfileServiceStub;
+import com.example.projecttwobase.service.UserServiceStub;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class UserProfileControllerTest {
-
-    private UserProfileController userProfileController;
-
     private UserProfileServiceImpl userProfileService;
 
     @Before
-    public void initializeUserProfileController(){
-        userProfileController = new UserProfileController();
-        userProfileController.setUserProfileService(new UserProfileServiceStub());
+    public void initializeUserProfile(){
+        userProfileService = new UserProfileServiceImpl(new UserServiceStub(), new UserProfileRepositoryStub());
     }
 
     @Test
     public void createUserProfile_AddsProfile_Success(){
+
         UserProfile userProfile = new UserProfile();
         userProfile.setEmail("batman@superhero.com");
+
         UserProfile newProfile = userProfileService.createUserProfile("batman", userProfile);
+
         Assert.assertNotNull(newProfile);
         Assert.assertEquals(newProfile.getEmail(), userProfile.getEmail());
+    }
+
+    @Test
+    public void getUserProfile_RetrievesProfileByUsername_Success(){
+
     }
 
 }
