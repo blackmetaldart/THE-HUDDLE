8c28972864fd7a22839444c98015fd7f9184b0a6
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:06:58 -0400

    Fixed the User Controller Test file

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserControllerTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserControllerTest.java
@@ -5,23 +5,34 @@ import com.example.projecttwobase.service.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.junit.runner.RunWith;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
-
 public class UserControllerTest {
+
     /**Main entry point for server-side Spring MVC test support.**/
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private JwtUtil jwtUtil;
 
     @Test
     public void helloWorld_ReturnsString_Success() throws Exception {
@@ -34,9 +45,26 @@ public class UserControllerTest {
                 .andExpect(content().string("Hello World!!"));
     }
 
-    @MockBean
-    private UserService userService;
+    @Test
+    public void login_Success() throws Exception{
 
-    @MockBean
-    private JwtUtil jwtUtil;
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("joe","abc"));
+
+        when(userService.login(any())).thenReturn("123456");
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"token\":\"123456\"}"))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+
+    private static String createUserInJson (String name, String password) {
+        return "{ \"name\": \"" + name + "\", " +
+                "\"password\":\"" + password + "\"}";
+    }
 }
