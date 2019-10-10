90b9a7b36f62cb307ed1189f1a6c7d15dc92945c
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:19:55 -0400

    worked on user profile controller test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
@@ -3,15 +3,6 @@ package com.example.projecttwobase.controller;
 import com.example.projecttwobase.model.UserProfile;
 
 public class UserProfileControllerTest {
-    @Override
-    public UserProfile createUserProfile(String username, UserProfile newProfile) {
-        return null;
-    }
-
-    @Override
-    public UserProfile getUserProfile(String username) {
-        return null;
-    }
     private UserProfileController userProfileController;
 
 }
