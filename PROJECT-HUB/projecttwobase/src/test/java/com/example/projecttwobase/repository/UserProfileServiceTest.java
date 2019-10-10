61b38201a3140c63f0fa56da4e8a53d81fb6772b
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 15:01:08 -0400

    Worked on user profile service test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/repository/UserProfileServiceTest.java
@@ -5,11 +5,26 @@ import com.example.projecttwobase.repository.UserProfileRepositoryStub;
 
 import com.example.projecttwobase.service.UserProfileServiceImpl;
 import com.example.projecttwobase.service.UserServiceStub;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class UserProfileServiceTest {
     @Before
     public void initializeUserProfile(){
         UserProfileServiceImpl userProfileService = new UserProfileServiceImpl(new UserServiceStub(), new UserProfileRepositoryStub());
+    }
+
+    @Test
+    public void createUserProfile_AddsProfile_Success(){
+
+        UserProfile userProfile = new UserProfile();
+        userProfile.setEmail("batman@superhero.com");
+
+        UserProfileServiceImpl userProfileService = null;
+        UserProfile newProfile = userProfileService.createUserProfile("batman", userProfile);
+
+        Assert.assertNotNull(newProfile);
+        Assert.assertEquals(newProfile.getEmail(), userProfile.getEmail());
     }
 }
