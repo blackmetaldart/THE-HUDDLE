f3a606248007c46cd866ce2b64eab3afb87d50a5
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:43:06 -0400

    Fixed user profile service stub

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/service/UserProfileServiceStub.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/service/UserProfileServiceStub.java
@@ -3,28 +3,14 @@ package com.example.projecttwobase.service;
 import com.example.projecttwobase.model.UserProfile;
 
 public class UserProfileServiceStub implements UserProfileService {
-
-//    @Override
-//    public UserProfile createUserProfile(String username, UserProfile newProfile) {
-//        return null;
-//    }
-
-    @Override
-    public UserProfile getUserProfile(String username) {
-        return null;
-    }
-
-    @Override
-    public UserProfile updateUserProfile(String unknown) {
-        return null;
-    }
-
     @Override
     public UserProfile createUserProfile(String username, UserProfile newProfile) {
         UserProfile userProfile = new UserProfile();
         userProfile.setEmail("batman@superhero.com");
-
         return userProfile;
     }
-
+    @Override
+    public UserProfile getUserProfile(String username) {
+        return null;
+    }
 }
