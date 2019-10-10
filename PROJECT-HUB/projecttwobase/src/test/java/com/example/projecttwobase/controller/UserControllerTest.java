458029c39612e925485e64f66787bdd13136cc71
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:52:57 -0400

    Fixed user controller test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserControllerTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserControllerTest.java
@@ -5,20 +5,19 @@ import com.example.projecttwobase.service.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.junit.runner.RunWith;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -66,5 +65,24 @@ public class UserControllerTest {
     private static String createUserInJson (String name, String password) {
         return "{ \"name\": \"" + name + "\", " +
                 "\"password\":\"" + password + "\"}";
+
+    }
+
+    @Test
+    public void signUp_Success() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createUserInJson("joe","abc"));
+
+        when(userService.login(any())).thenReturn("123456");
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"token\":\"123456\"}"))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
     }
 }
