5cdeb3e5f85eac28dfefa56ff63fdacf1ec57d38
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:20:59 -0400

    created user profile service stub and added code

---
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/service/UserProfileServiceStub.java
@@ -0,0 +1,17 @@
+package com.example.projecttwobase.service;
+
+import com.example.projecttwobase.model.UserProfile;
+
+public class UserProfileServiceStub implements UserProfileService {
+
+    @Override
+    public UserProfile createUserProfile(String username, UserProfile newProfile) {
+        return null;
+    }
+
+    @Override
+    public UserProfile getUserProfile(String username) {
+        return null;
+    }
+
+}
