e5826575b2bf5875c42367d726d3e2370fa1cd5b
Alexander <mrbeachcode@gmail.com>
2019-10-10 12:23:11 -0400

    Found the bug in the post's username setter method

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/Post.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/model/Post.java
@@ -34,7 +34,7 @@ public class Post {
     public void setUser(User user){this.user = user;}
 
     public String getUsername() {return username; }
-    public void setUsername(String username) { this.username = getUsername();}
+    public void setUsername(String username) { this.username = username;}
 
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
