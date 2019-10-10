3f80b8ccf6a67ca5e6fe4d17107215bb52d74d5b
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:12:37 -0400

    Added service test file

---
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/service/UserServiceTest.java
@@ -0,0 +1,75 @@
+package com.example.projecttwobase.service;
+
+import com.example.projecttwobase.config.JwtUtil;
+import com.example.projecttwobase.model.User;
+import com.example.projecttwobase.repository.PostRepository;
+import com.example.projecttwobase.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRoleService userRoleService;
+
+    @Mock
+    private PostService postService;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private User user;
+
+    @Before
+    public void initializeDummyUser(){
+        user.setUsername("batman");
+        user.setPassword("robin");
+    }
+
+    @Test
+    public void getUser_ReturnsUser_Success(){
+
+        when(userRepository.findByUsername(anyString())).thenReturn(user);
+
+        User tempUser = userService.getUser(user.getUsername());
+
+        assertEquals(tempUser.getUsername(), user.getUsername());
+    }
+
+    @Test
+    public void login_UserNotFound_Error(){
+
+        when(userRepository.findByUsername(anyString())).thenReturn(null);
+
+        String token = userService.login(user);
+
+        assertEquals(token, null);
+    }
+}
