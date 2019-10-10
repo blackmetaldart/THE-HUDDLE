b6b7892f426d72d5e98f5cfa005445a8df06de4e
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:23:14 -0400

    Added code to user profile controller test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
@@ -3,6 +3,7 @@ package com.example.projecttwobase.controller;
 import com.example.projecttwobase.model.UserProfile;
 import com.example.projecttwobase.service.UserProfileServiceStub;
 import org.junit.Before;
+import org.junit.Test;
 
 public class UserProfileControllerTest {
     private UserProfileController userProfileController;
@@ -11,6 +12,17 @@ public class UserProfileControllerTest {
     public void initializeUserProfileController(){
         userProfileController = new UserProfileController();
         userProfileController.setUserProfileService(new UserProfileServiceStub());
+    }
+
+    @Test
+    public void createUserProfile_SaveUserProfile_Success(){
+        UserProfile userProfile = new UserProfile();
+        userProfile.setEmail("batman@superhero.com");
+
+        UserProfile newProfile = userProfileController.createUserProfile("batman", userProfile);
+
+        Assert.assertNotNull(newProfile);
+        Assert.assertEquals(newProfile.getEmail(), userProfile.getEmail());
     }
 
 }
