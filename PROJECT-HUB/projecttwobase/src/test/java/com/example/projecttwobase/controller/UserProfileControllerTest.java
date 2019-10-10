4dc27d28834c7761c2d499d14435b3df7f1cc66c
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:22:08 -0400

    Added @before to usee profile controller test

--- PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/controller/UserProfileControllerTest.java
@@ -1,8 +1,16 @@
 package com.example.projecttwobase.controller;
 
 import com.example.projecttwobase.model.UserProfile;
+import com.example.projecttwobase.service.UserProfileServiceStub;
+import org.junit.Before;
 
 public class UserProfileControllerTest {
     private UserProfileController userProfileController;
+
+    @Before
+    public void initializeUserProfileController(){
+        userProfileController = new UserProfileController();
+        userProfileController.setUserProfileService(new UserProfileServiceStub());
+    }
 
 }
