0c52684f1751acf712cbb4f64418d30a8378d05c
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 14:41:01 -0400

    Added user service stub test file

---
+++ PROJECT-HUB/projecttwobase/src/test/java/com/example/projecttwobase/service/UserServiceStub.java
@@ -0,0 +1,44 @@
+package com.example.projecttwobase.service;
+
+import com.example.projecttwobase.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserServiceStub implements UserService {
+
+    @Override
+    public User getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public Iterable<User> listUsers() {
+        return null;
+    }
+
+    @Override
+    public String createUser(User newUser) {
+        return null;
+    }
+
+    @Override
+    public String login(User user) {
+        return null;
+    }
+
+    @Override
+    public HttpStatus deleteById(Long userId) {
+        return null;
+    }
+
+    @Override
+    public User addSong(String username, Long songId) {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
