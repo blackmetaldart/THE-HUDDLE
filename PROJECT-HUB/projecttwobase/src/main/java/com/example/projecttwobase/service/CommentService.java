5c2f5219affd57b40e1e83ee0db09be558b4a49b
Zainab Akrami <zainabakrami21@gmail.com>
2019-10-10 10:51:48 -0400

    Added to comment service

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/CommentService.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/CommentService.java
@@ -3,8 +3,10 @@ package com.example.projecttwobase.service;
 import com.example.projecttwobase.model.Comment;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface CommentService {
     Comment createComment(Comment comment, Long postId);
-   // List<Comment> getCommentByUsername (String username);
+    List<Comment> getCommentByPostId (Long postId);
     ResponseEntity<Object> deleteCommentByCommentId (Long commentId);
 }
