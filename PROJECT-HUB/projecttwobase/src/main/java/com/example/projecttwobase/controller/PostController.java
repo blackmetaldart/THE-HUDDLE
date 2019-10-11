ca951f5a6b19a75c59b95d0c0ef9b40c13b010d4
Alexander <mrbeachcode@gmail.com>
2019-10-11 13:47:11 -0400

    commented the post controller for readability

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/PostController.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/PostController.java
@@ -15,30 +15,30 @@ public class PostController {
     @Autowired
     PostService postService;
 
-    //ALLOWS A USER TO CREATE A POST / REFER TO POST SERVICE
+    //ENDPOINT THAT ALLOWS A USER TO CREATE A POST / REFER TO POST SERVICE
     @PostMapping("{username}/makepost")
     public Post createPost(@Valid @RequestBody Post post, @Valid @PathVariable String username){
         return postService.createPost(username, post);
     }
 
-    //ALLOWS A USER TO GET A LIST OF ALL POSTS / REFER TO POST SERVICE
+    //ENDPOINT THAT ALLOWS A USER TO GET A LIST OF ALL POSTS / REFER TO POST SERVICE
     @GetMapping("/posts/list")
     public Iterable<Post> listPosts(){
         return postService.listAllPosts();
     }
 
-    //ALLOWS A USER TO GET A POST BY POST ID / REFER TO POST SERVICE
+    //ENDPOIT THAT ALLOWS A USER TO GET A POST BY POST ID / REFER TO POST SERVICE
     @GetMapping("/post/{postId}")
     public Post postsById(@Valid @PathVariable Long postId) {
         return postService.getPostByPostId(postId);
     }
 
-    //ALLOWS A USER TO DELETE A POST BY POST ID / REFER TO POST SERVICE
+    //ENDPOINT THAT ALLOWS A USER TO DELETE A POST BY POST ID / REFER TO POST SERVICE
     @DeleteMapping ("/post/{postId}")
     public ResponseEntity<Object> deletePostByPostId(@PathVariable Long postId) { return postService.deletePostByPostId(postId);
     }
 
-    //ALLOWS A USER TO GET ALL POSTS BY A USER ID / REFER TO POST SERVICE
+    //ENDPOINT THAT ALLOWS A USER TO GET ALL POSTS BY A USER ID / REFER TO POST SERVICE
     @GetMapping("/{username}/posts")
     public List<Post> getPostsByUsername (@PathVariable String username) {
         return postService.getPostsByUsername(username);
