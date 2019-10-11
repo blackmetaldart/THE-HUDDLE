5d495a4e8b2335fac9b3a4efc276d8a493449e28
Alexander <mrbeachcode@gmail.com>
2019-10-11 13:41:46 -0400

    commented the post controller for readability

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/PostController.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/controller/PostController.java
@@ -15,30 +15,30 @@ public class PostController {
     @Autowired
     PostService postService;
 
-    //CREATE POST
+    //CREATE POST / REFER TO POST SERVICE
     @PostMapping("{username}/makepost")
     public Post createPost(@Valid @RequestBody Post post, @Valid @PathVariable String username){
         return postService.createPost(username, post);
     }
 
-    //GET LIST OF ALL POSTS
+    //GET LIST OF ALL POSTS / REFER TO POST SERVICE
     @GetMapping("/posts/list")
     public Iterable<Post> listPosts(){
         return postService.listAllPosts();
     }
 
-    //GET POST BY POST ID
+    //GET POST BY POST ID / REFER TO POST SERVICE
     @GetMapping("/post/{postId}")
     public Post postsById(@Valid @PathVariable Long postId) {
         return postService.getPostByPostId(postId);
     }
 
-    //DELETE POST BY POST ID
+    //DELETE POST BY POST ID / REFER TO POST SERVICE
     @DeleteMapping ("/post/{postId}")
     public ResponseEntity<Object> deletePostByPostId(@PathVariable Long postId) { return postService.deletePostByPostId(postId);
     }
 
-    //GET ALL POSTS BY USER ID
+    //GET ALL POSTS BY USER ID / REFER TO POST SERVICE
     @GetMapping("/{username}/posts")
     public List<Post> getPostsByUsername (@PathVariable String username) {
         return postService.getPostsByUsername(username);
