5be1b7eb49507a70768bf67939a4f95c159153e2
Alexander <mrbeachcode@gmail.com>
2019-10-11 14:21:00 -0400

    commented the post service implementation for readability

--- PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostServiceImpl.java
+++ PROJECT-HUB/projecttwobase/src/main/java/com/example/projecttwobase/service/PostServiceImpl.java
@@ -21,9 +21,12 @@ public class PostServiceImpl implements PostService{
         return postRepository.save(post);
     }
 
+    //GETS A POST FROM THE POST REPOSITORY USING THE POST ID
     public Post getPostByPostId (Long postId) {
-        return postRepository.getPostById(postId);}
+        return postRepository.getPostById(postId);
+    }
 
+    //DELETES A POST FROM THE POST REPOSITORY
     public ResponseEntity<Object> deletePostByPostId(Long postId){
        return postRepository.findById(postId).map (post -> {
             postRepository.delete(post);
@@ -31,10 +34,12 @@ public class PostServiceImpl implements PostService{
         }).orElseThrow(() -> new ExceptionHandler("PostId " + postId + " not found"));
     }
 
+    //GETS A POST USING FROM THE POST REPOSITORY USING THE USERNAME
     public List<Post> getPostsByUsername(String username) {
         return postRepository.findAllByUsername(username);
     }
 
+    //LISTS ALL THE POSTS IN THE REPOSITORY
     @Override
     public Iterable<Post> listAllPosts(){
         return postRepository.findAll();
