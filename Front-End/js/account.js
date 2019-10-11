2b111af7a552c573a8f0d69dd30753b9d81748e1
Alexander <mrbeachcode@gmail.com>
2019-10-11 13:14:00 -0400

    commented the account js for readability

--- Front-End/js/account.js
+++ Front-End/js/account.js
@@ -1,18 +1,17 @@
 //DISPLAYS USERNAME IN NAV
 let accountLink = document.querySelector('#account');
-
 let usernameDisplay = localStorage.getItem('username');
 
 accountLink.innerText = usernameDisplay;
 accountLink.style.borderBottom = "3px double rgb(163,13,45)";
 
 //===ACCOUNT/PROFILE===//
-//create profile form variables
+//CREATE PROFILE FORM VARIABLES
 let addEmail = document.getElementById('addEmail');
 let mobile = document.getElementById('mobile');
 let addy = document.getElementById('addy');
 
-//display profile variables
+//DISPLAY PROFLIE VARIABLES
 const displayUserName = document.getElementById('displayUserName');
 const displayPrimaryEmail = document.getElementById('displayPrimaryEmail');
 const displayAddEmail = document.getElementById('displayAddEmail');
@@ -21,26 +20,20 @@ const displayAddy = document.getElementById('displayAddy');
 
 displayProfile();
 
-//shows profile information
+//SHOWS PROFILE INFORMATION
 function displayProfile() {
 
   fetch('http:localhost:8080/profile', {
     method: 'GET',
     headers: {
       "Authorization": "Bearer " + localStorage.getItem('user'),
-      "Content-Type": "application/json"
-    }
-  })
-
-  .then((res) => {
-    return res.json();
-  })
-
+      "Content-Type": "application/json"}})
+  .then((res) => {return res.json();})
   .then((res) => {
     displayUserName.innerHTML = localStorage.getItem('username');
     displayPrimaryEmail.innerHTML = localStorage.getItem('primaryEmail');
 
-
+    //DISPLAYS THE USER'S EMAIL IF THERE IS ONE
     if(res.additionalEmail === undefined || !localStorage.getItem('secondaryEmail')) {
       displayAddEmail.innerHTML = "<em>Add Secondary Email</em>";
     } else if(res.additionalEmail) {
@@ -50,6 +43,7 @@ function displayProfile() {
       displayAddEmail.innerHTML = localStorage.getItem('secondaryEmail');
     }
 
+    //DISPLAYS THE USER'S MOBILE IF THERE IS ONE
     if(res.mobile === undefined || !localStorage.getItem('mobile')) {
       displayMobile.innerHTML = "<em>Add Mobile Number</em>";
     } else if(res.mobile) {
@@ -59,6 +53,7 @@ function displayProfile() {
       displayMobile.innerHTML = localStorage.getItem('mobile');
     }
 
+    //DISPLAYS THE USER'S ADDRESS IF THERE IS ONE
     if(res.address === undefined || !localStorage.getItem('addy')) {
       displayAddy.innerHTML = "<em>Add Home Address</em>";
     } else if(res.address) {
@@ -66,22 +61,19 @@ function displayProfile() {
       localStorage.setItem('addy', res.address);
     } else if(!res.address) {
       displayAddy.innerHTML = localStorage.getItem('addy');
-    }
+    }})
 
-})
-
-  .catch((err) => {
-      console.log(err);
-  })
+  //LOGS ERRORS TO CONSOLE
+  .catch((err) => {console.log(err);})
 
 }
 
-//CREATE PROFILE FUNCTION - used also as a workaround for the update profile
-  //create profile button
+//CREATE PROFILE FUNCTION - USED ALSO AS A WORKAROUND FOR THE UPDATE PROFILE
+  //CREATE PROFILE BUTTON
 const createProfileBtn = document.getElementById("createProfileBtn");
 createProfileBtn.addEventListener("click", createProfile);
 
-  //create profile function
+  //CREATE PROFILE FUNCTION
 function createProfile(e){
   e.preventDefault();
 
@@ -89,47 +81,43 @@ function createProfile(e){
     method: 'POST',
     headers: {
       "Authorization": "Bearer " + localStorage.getItem('user'),
-      "Content-Type": "application/json"
-    },
+      "Content-Type": "application/json"},
     body: JSON.stringify({
       additionalEmail: addEmail.value,
       mobile: mobile.value,
-      address: addy.value
-    })
+      address: addy.value})})
 
-  })
+  .then((res) => {return res.json();})
 
-  .then((res) => {
-    return res.json();
-  })
+  //CALLS UPDATEPROFILE FUNCTION TO SHOW NEW CHANGES FROM "CREATE/UPDATE" PROFILE FORM
+  .then((res)=> {updateProfile(res);})
 
-  //calls updateProfile function to show new changes from "create/update" profile form
-  .then((res)=> {
-  updateProfile(res);
-  })
-
-  .catch((err) => {
-      console.log(err);
-  })
+  //LOGS ERRORS TO CONSOLE
+  .catch((err) => {console.log(err);})
 
 }
 
-  //updates profile function
+  //UPDATES PROFILE FUNCTION
 function updateProfile(res) {
 
-  //if input value is filled, then don't update form
+  //IF INPUT VALUE FOR THE USER'S ADDITIONAL EMAIL IS AN EMPTY STRING OR NULL, THEN DON'T UPDATE FORM
+  //OTHERWISE DISPLAY THE USER'S ADDITIONAL EMAIL
   if(addEmail.value === "" || addEmail.value === null) {
     return;
   } else {
     displayAddEmail.innerHTML = res.additionalEmail;
   }
 
+  //IF INPUT VALUE FOR THE USER'S MOBILE IS AN EMPTY STRING OR NULL, THEN DON'T UPDATE FORM
+  //OTHERWISE DISPLAY THE USER'S MOBILE
   if(mobile.value === "" || mobile.value === null) {
     return;
   } else {
     displayMobile.innerHTML = res.mobile;
   }
 
+  //IF INPUT VALUE FOR THE USER'S ADDRESS IS AN EMPTY STRING OR NULL, THEN DON'T UPDATE FORM
+  //OTHERWISE DISPLAY THE USER'S ADDRESS
   if(addy.value === "" || mobile.value === null) {
     return;
   } else {
