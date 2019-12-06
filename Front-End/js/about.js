eb2c2bcfc07f591709b3d523b5a17de3d2da5347
Alexander <mrbeachcode@gmail.com>
2019-12-05 22:24:00 -0500

    the username doesn't display on the account navbar

--- Front-End/js/about.js
+++ Front-End/js/about.js
@@ -1,10 +1,10 @@
-//DISPLAYS USERNAME IN NAV
-let accountLink = document.querySelector('#account');
-
-let usernameDisplay = localStorage.getItem('username');
-
+// ADDS FUNCTION TO THE LOGOUT BUTTON
 const logoutBtn = document.querySelector('.logout');
 logoutBtn.addEventListener('click', logout);
+
+// DISPLAYS USERNAME IN NAV
+let accountLink = document.querySelector('#account');
+let usernameDisplay = localStorage.getItem('username');
 
 accountLink.innerText = usernameDisplay;
 accountLink.style.borderBottom = "3px double rgb(163,13,45)";
