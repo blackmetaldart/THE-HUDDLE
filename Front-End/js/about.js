8f28080bed1303dfd383c6ffa48bd1f8bebfae5c
Alexander <mrbeachcode@gmail.com>
2019-12-05 22:01:59 -0500

    added the logout function to the account page

--- Front-End/js/about.js
+++ Front-End/js/about.js
@@ -5,3 +5,10 @@ let usernameDisplay = localStorage.getItem('username');
 
 accountLink.innerText = usernameDisplay;
 accountLink.style.borderBottom = "3px double rgb(163,13,45)";
+
+// THIS FUNCTION WILL LOGOUT THE USER
+function logout(){
+  localStorage.setItem('user', null);
+  localStorage.setItem('username', null);
+  localStorage.setItem('primaryEmail', null);
+}
