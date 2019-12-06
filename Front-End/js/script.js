2c1451d2c077bcd4f5e54cd562240f252498af13
Alexander <mrbeachcode@gmail.com>
2019-12-05 22:36:58 -0500

    formatted the comments and set the token in one line instead of two

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -1,29 +1,29 @@
-//VARIABLE FOR THE JSON WEB TOKEN
+// VARIABLE FOR THE JSON WEB TOKEN
 let token;
 
-//THESE VARIABLES STORE THE ELEMENTS FOR THE LOGIN AND SIGNUP FORM
+// THESE VARIABLES STORE THE ELEMENTS FOR THE LOGIN AND SIGNUP FORM
 const signUpBtn = document.querySelector('#signUpBtn');
 const logInBtn = document.querySelector('#logInBtn');
 const signUpForm = document.querySelector('#signUpForm');
 const logInForm = document.querySelector('#logInForm');
 const signUpSubmit = document.querySelector(".signUpSubmit");
 const logInSubmit = document.querySelector(".logInSubmit");
-//THESE VARIABLES STORE THE ELEMENTS THAT HOLD THE DATA FOR REGISTERING THE USER
+// THESE VARIABLES STORE THE ELEMENTS THAT HOLD THE DATA FOR REGISTERING THE USER
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const username = document.querySelector('.userName');
-//THESE VARIABLES STORE THE DATA THAT THE USER LOGS IN WITH
+// THESE VARIABLES STORE THE DATA THAT THE USER LOGS IN WITH
 const logInUsername = document.querySelector('.logInUsername');
 const logInPassword = document.querySelector('.logInPassword');
 
-//THIS SECTION WILL HOLD ALL OF THE EVENT LISTENERS TO AFFECT THE DISPLAY OF THE APPLICATION
+// THIS SECTION WILL HOLD ALL OF THE EVENT LISTENERS TO AFFECT THE DISPLAY OF THE APPLICATION
 signUpSubmit.addEventListener('click', signUp);
 signUpBtn.addEventListener('click', signUpToggle);
 logInBtn.addEventListener('click', logInToggle);
 logInSubmit.addEventListener('click', logIn);
 
 
-//THIS FUNCTION ALLOWS THE SIGNUP MENU TO TOGGLE
+// THIS FUNCTION ALLOWS THE SIGNUP MENU TO TOGGLE
 function signUpToggle() {
   signUpForm.classList.toggle('none');
   if(signUpForm.classList.contains('none')) {
@@ -33,7 +33,7 @@ function signUpToggle() {
   }
 }
 
-//THIS FUNCTION ALLOWS THE LOGIN MENU TO TOGGLE
+// THIS FUNCTION ALLOWS THE LOGIN MENU TO TOGGLE
 function logInToggle() {
   logInForm.classList.toggle('none');
   if(logInForm.classList.contains('none')) {
@@ -43,7 +43,7 @@ function logInToggle() {
   }
 }
 
-//THIS FUNCTION ALLOWS THE USERS TO BE CREATED THROUGH SIGNUP
+// THIS FUNCTION ALLOWS THE USERS TO BE CREATED THROUGH SIGNUP
 function signUp(e) {
   e.preventDefault();
 
@@ -62,16 +62,14 @@ function signUp(e) {
           })})
   .then((response) => {return response.json();})
   .then((response) => {
-    token = response.token;//store token value to variable token
-    //allows for data persistence between html pages
-    localStorage.setItem('user', token); //stores token as a cookie
+    localStorage.setItem('user', response.token);
 
-    //REDIRECTS USER TO HOMEPAGE
+    // REDIRECTS USER TO HOMEPAGE
     redirectHome();})
   .catch((error) => {console.log(error);})
 }
 
-//THIS FUNCTION RETURNS A USER UPON LOGIN
+// THIS FUNCTION RETURNS A USER UPON LOGIN
 function logIn(e) {
     e.preventDefault();
 
@@ -91,7 +89,7 @@ function logIn(e) {
       .catch((error) => {console.error(error);});
   }
 
-//THIS FUNCTION REDIRECTS A USER TO THE HOMEPAGE
+// THIS FUNCTION REDIRECTS A USER TO THE HOMEPAGE
 function redirectHome() {
     if (token != null) {
     window.location.href = "./home.html";
