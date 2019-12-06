c7579e33b517696adc875ff8931f5e66a6595dea
Alexander <mrbeachcode@gmail.com>
2019-12-05 22:03:05 -0500

    added the query and event listener to the about js

--- Front-End/js/about.js
+++ Front-End/js/about.js
@@ -3,6 +3,9 @@ let accountLink = document.querySelector('#account');
 
 let usernameDisplay = localStorage.getItem('username');
 
+const logoutBtn = document.querySelector('.logout');
+logoutBtn.addEventListener('click', logout);
+
 accountLink.innerText = usernameDisplay;
 accountLink.style.borderBottom = "3px double rgb(163,13,45)";
 
